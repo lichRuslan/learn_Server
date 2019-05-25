f1fafba1a64efaf90f02d7d2c4bcc5544764a0d7
lichruslan <lichruslan@github.com>
2019-05-25 23:46:08 +0600

    add create, add and delete

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 let express = require('express');
+let bodyParser = require('body-parser');
+
 let app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true})); //что парсить данные формы
 let users = [
     {id : 1, name : "Ruslan"},
     {id : 2, name : "Tamerlan"},
@@ -21,6 +25,32 @@ app.get('/users/:id', (req,res)=>{
     if(user) res.send(user);
     else res.send('К сожилению такого пользователя нет');
 });
+
+//Добовление пользователя
+app.post('/users', (req,res)=>{
+    let user = {
+        id: Date.now(),
+        name: req.body.name
+    };
+    users.push(user);
+    res.send(user);
+});
+// Редактирование пользователя
+app.put('/users/:id', (req,res)=>{
+    let user = users.find((user)=>{
+        return user.id === Number(req.params.id);
+    });
+    user.name = req.body.name;
+    res.sendStatus(200);
+});
+// Удаление пользователя
+app.delete('/users/:id', (req,res)=>{
+    users = users.filter((user)=>{
+        return user.id !== Number(req.params.id);
+    });
+    res.sendStatus(200);
+});
+
 app.listen(3000, ()=>{
     console.log('SERVER START');
 });
